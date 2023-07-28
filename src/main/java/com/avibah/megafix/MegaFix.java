c6a38e23a8daba92818d7d5fb7114b7f8aad77a5
Avibah <redstoneminecraft45@gmail.com>
2023-07-28 00:16:34 -0500

    Finalize script/configuration

--- src/main/java/com/avibah/megafix/MegaFix.java
+++ src/main/java/com/avibah/megafix/MegaFix.java
@@ -1,28 +1,52 @@
 package com.avibah.megafix;
 
+import com.pixelmonmod.pixelmon.api.config.PixelmonConfigProxy;
+import com.pixelmonmod.pixelmon.spawning.LegendarySpawner;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraftforge.event.world.WorldEvent;
-import com.pixelmonmod.pixelmon.spawning.LegendarySpawner;
 import com.pixelmonmod.pixelmon.spawning.PixelmonSpawning;
 
 @Mod("megafix")
 public class MegaFix
 {
     private static final Logger LOGGER = LogManager.getLogger();
+    private boolean fixEnabled = false;
+
+    private LegendarySpawner spawner;
+    private float tickRatio;
+    private long prevSpawnTime;
 
     public MegaFix() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    private void onWorldLoad(WorldEvent.Load event) {
-        LegendarySpawner spawner = PixelmonSpawning.megaBossSpawner;
-        spawner.firesChooseEvent = true;
+    public void onServerTick(TickEvent.ServerTickEvent event) {
+        // enable bosses and prep values on first tick
+        if (!fixEnabled) {
+            spawner = PixelmonSpawning.megaBossSpawner;
+            spawner.firesChooseEvent = true;
 
-        LOGGER.info("Enabled Mega Boss spawner");
+            int legendaryTicks = PixelmonConfigProxy.getSpawning().getLegendarySpawnTicks();
+            int bossTicks = PixelmonConfigProxy.getSpawning().getBossSpawning().getBossSpawnTicks();
+
+            tickRatio = (float)bossTicks / legendaryTicks;
+            prevSpawnTime = spawner.nextSpawnTime;
+
+            fixEnabled = true;
+            LOGGER.info("Enabled Mega Boss fix");
+        }
+
+        // casual fighting the spawner frequency logic
+        if (spawner.nextSpawnTime != prevSpawnTime) {
+            long diff = spawner.nextSpawnTime - spawner.lastCycleTime;
+
+            spawner.nextSpawnTime = spawner.lastCycleTime + (long)(diff * tickRatio);
+            prevSpawnTime = spawner.nextSpawnTime;
+        }
     }
 }
